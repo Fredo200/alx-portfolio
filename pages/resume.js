30dbb2d40edd60dcda6304ca8eae0c95717727e8
Fredo200 <142917109+Fredo200@users.noreply.github.com>
2024-12-29 08:07:32 +0300

    Adding Custom Cursor Toggle

--- pages/resume.js
+++ pages/resume.js
@@ -10,7 +10,7 @@ import { useTheme } from "next-themes";
 import { name, showResume } from "../data/portfolio.json";
 import { resume } from "../data/portfolio.json";
 import data from "../data/portfolio.json";
-
+// Resume
 const Resume = () => {
   const router = useRouter();
   const theme = useTheme();
